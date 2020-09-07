24bbe61e261f15b5440559bbbfc760a3d84cde4d
James Sinkler <jamessinkler713@gmail.com>
2020-09-06 18:17:47 -0700

    moves some boilerplate around to match up pet app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+//added for graphql
+import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
+import client from './client'
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
